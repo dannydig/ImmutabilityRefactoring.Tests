72937e90f6ce91b33a9c2611c73af92add990bf9
fredrikbk <fredrikbk@gmail.com>
2009-10-17 15:46:17 -0500

    New and better version of the uninitialized variables test

--- resources/SimpleFields/object_in/TestUninitializedFieldsIntoFinal.java
+++ resources/SimpleFields/object_in/TestUninitializedFieldsIntoFinal.java
@@ -6,21 +6,26 @@ public class TestUninitializedFieldsIntoFinal {
 	int b, c;
 	
 	// Check non-ints
-	float d;
-	double e;
-	String f;
-	Object g;
+	boolean d;
+	byte e;
+	char f; 
+	float g;
+	double h;
+	short i;
+	long j;
+	String k;
+	Object l;
 	
 	// Check that it does not destroy the present initialization in a list where only one is initialized
-	int h, i = 42;
+	int m, n = 42;
 	
 	// Check that it does not initialize values that are initialized in a constructor
-	int j;
-	TestUninitializedFieldsIntoFinal(int j) {
-		this.j = j;
+	int o;
+	TestUninitializedFieldsIntoFinal(int o) {
+		this.o = o;
 	}
 	
-	// Check that it adds an initialization of j to a second constructor so that it is initialized in all cases
+	// Check that it adds an initialization of o to a second constructor so that it is initialized in all cases
 	TestUninitializedFieldsIntoFinal(float irrelevant) {
 			
 	}
--- resources/SimpleFields/object_out/TestUninitializedFieldsIntoFinal.java
+++ resources/SimpleFields/object_out/TestUninitializedFieldsIntoFinal.java
@@ -6,22 +6,27 @@ public class TestUninitializedFieldsIntoFinal {
 	final int b = 0, c = 0;
 	
 	// Check non-ints
-	final float d = 0.0f;
-	final double e = 0.0;
-	final String f = "";
-	final Object g = null;
+	final boolean d = false;
+	final byte e = 0;
+	final char f = '\0'; 
+	final float g = 0.0f;
+	final double h = 0.0d;
+	final short i = 0;
+	final long j = 0;
+	final String k = "";
+	final Object l = null;
 	
 	// Check that it does not destroy the present initialization in a list where only one is initialized
-	final int h = 0, i = 42;
+	final int m = 0, n = 42;
 	
 	// Check that it does not initialize values that are initialized in a constructor
-	final int j;
-	TestUninitializedFieldsIntoFinal(int j) {
-		this.j = j;
+	final int o;
+	TestUninitializedFieldsIntoFinal(int o) {
+		this.o = o;
 	}
 	
-	// Check that it adds an initialization of j to a second constructor so that it is initialized in all cases
+	// Check that it adds an initialization of o to a second constructor so that it is initialized in all cases
 	TestUninitializedFieldsIntoFinal(float irrelevant) {
-		this.j = 0;
+		this.o = 0;	
 	}
 }
